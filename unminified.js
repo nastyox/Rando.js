91d3f02a7078e33eef957291c81f9a621a985b45
nastyox <61916140+nastyox@users.noreply.github.com>
2020-06-25 20:39:25 -0400

    Human-readable version of 1.0.0.js

--- unminified.js
+++ unminified.js
@@ -1,212 +1,131 @@
-function rando(min, max, allowDecimals){
-	function isUndefined(variable){
-		return typeof variable === "undefined";
-	}
-	
-	function isNumber(num){
-		return typeof num === "number" && !isNaN(num);
-	}
-	
-	function isString(str){
-		return typeof str === "string";
-	}
-	
-	function isObject(obj){
-		return typeof obj === "object";
-	}
-	
-	function isArray(arr){
-		return !isUndefined(arr) && arr !== null && arr.constructor === Array;
-	}
-	
-	try{
-		if(min !== null && max !== null && allowDecimals !== null){
-			if(isUndefined(min)){
-				return Math.random();
-			}
-			
-			if(!!window.jQuery && min instanceof jQuery && isUndefined(max)){
-				//jQuery object
-				if(min.length == 0){
-					return false;
-				}
-				
-				var index = rando(0, min.length - 1);
-				return {index:index, value:min.eq(index)};
-			}
-			
-			if(isNumber(min) && isNumber(max) && isString(allowDecimals) && allowDecimals.toLowerCase().trim() == "float"){
-				//double float
-				if(min > max){
-					var temp = max;
-					max = min;
-					min = temp;
-				}
-				
-				return Math.random() * (max - min) + min;
-			}
-			
-			if(isArray(min) && min.length > 0 && isUndefined(max)){
-				//array
-				var arr = min;
-				var pickedIndex = Math.random() * arr.length << 0;
-				return {index: pickedIndex, value: arr[pickedIndex]};
-			}
-			
-			if(isObject(min) && isUndefined(max)){
-				//object
-				var obj = min;
-				var keys = Object.keys(obj);
-				if(keys.length > 0){
-					var key = keys[keys.length * Math.random() << 0];
-   		 			return {key: key, value: obj[key]};
-   		 		}
-			}
-			
-			if(((min === true && max === false) || (min === false && max === true)) && isUndefined(allowDecimals)){
-				//bool
-				return rando() < .5;
-			}
-			
-			if(isNumber(min) && isUndefined(max)){
-				//single int
-				if(min >= 0){
-					return rando(0, min);
-				}
-				return rando(min, 0);
-			}
-			
-			if(isNumber(min) && isString(max) && max.toLowerCase().trim() == "float" && isUndefined(allowDecimals)){
-				//single float
-				if(min >= 0){
-					return rando(0, min, "float");
-				}
-				return rando(min, 0, "float");
-			}
-			
-			if(isNumber(min) && isNumber(max) && isUndefined(allowDecimals)){
-				//double int
-				if(min > max){
-					var temp = max;
-					max = min;
-					min = temp;
-				}
-				
-				min = Math.floor(min);
-				max = Math.floor(max);
-   			 	return Math.floor(Math.random() * (max - min + 1) + min);
-			}
-			
-			if(isString(min) && min.length > 0 && isUndefined(max)){
-				//string
-				return min.charAt(rando(0, min.length - 1));
-			}
-		}
-		return false;
-	}
-	catch(err){
-		return false;
-	}
+function rando(arg1, arg2, arg3){
+    var isUndefined = (variable) => typeof variable === "undefined",
+    isNumber = (num) => typeof num === "number" && !isNaN(num),
+    isString(str) => typeof str === "string",
+    isObject(obj) => typeof obj === "object",
+    isArray(arr) => !isUndefined(arr) && arr !== null && arr.constructor === Array;
+    
+    try{
+        if(arg1 !== null && arg2 !== null && arg3 !== null){
+            if(isUndefined(arg1)){
+                //regular decimal
+                return Math.random();
+            }
+            
+            if(!!window.jQuery && arg1 instanceof jQuery && isUndefined(arg2)){
+                //jQuery object
+                if(arg1.length == 0) return false;
+                var index = rando(0, arg1.length - 1);
+                return {index:index, value:arg1.eq(index)};
+            }
+            
+            if(isNumber(arg1) && isNumber(arg2) && isString(arg3) && arg3.toLowerCase().trim() == "float"){
+                //float from min to max (inclusive of min and exclusive of max)
+                if(arg1 > arg2) var temp = arg2, arg2 = arg1, arg1 = temp;
+                return Math.random() * (arg2 - arg1) + arg1;
+            }
+            
+            if(isArray(arg1) && arg1.length > 0 && isUndefined(arg2)){
+                //array
+                var arr = arg1, pickedIndex = Math.random() * arr.length << 0;
+                return {index: pickedIndex, value: arr[pickedIndex]};
+            }
+            
+            if(isObject(arg1) && isUndefined(arg2)){
+                //object
+                var obj = arg1, keys = Object.keys(obj);
+                if(keys.length > 0){
+                    var key = keys[keys.length * Math.random() << 0];
+                    return {key: key, value: obj[key]};
+                }
+            }
+            
+            if(((arg1 === true && arg2 === false) || (arg1 === false && arg2 === true)) && isUndefined(arg3)){
+                //boolean
+                return rando() < .5;
+            }
+            
+            if(isNumber(arg1) && isUndefined(arg2)){
+                //int from 0 through max OR min through 0 if negative (inclusive of both min and max)
+                if(arg1 >= 0) return rando(0, arg1);
+                return rando(arg1, 0);
+            }
+            
+            if(isNumber(arg1) && isString(arg2) && arg2.toLowerCase().trim() == "float" && isUndefined(arg3)){
+                //float from 0 to max OR min to 0 if negative (inclusive of min and exclusive of max)
+                return arg1 >= 0 ? rando(0, arg1, "float") : rando(arg1, 0, "float");
+            }
+            
+            if(isNumber(arg1) && isNumber(arg2) && isUndefined(arg3)){
+                //int from min through max (inclusive of both min and max)
+                if(arg1 > arg2) var temp = arg2, arg2 = arg1, arg1 = temp;
+                arg1 = Math.floor(arg1), arg2 = Math.floor(arg2);
+                return Math.floor(Math.random() * (arg2 - arg1 + 1) + arg1);
+            }
+            
+            if(isString(arg1) && arg1.length > 0 && isUndefined(arg2)){
+                //string
+                return arg1.charAt(rando(0, arg1.length - 1));
+            }
+        }
+        return false;
+    }
+    catch(err) return false;
 }
 
-function randoSequence(min, max){
-	function isUndefined(variable){
-		return typeof variable === "undefined";
-	}
-	
-	function isNumber(num){
-		return typeof num === "number" && !isNaN(num);
-	}
-	
-	function isString(str){
-		return typeof str === "string";
-	}
-	
-	function isObject(obj){
-		return typeof obj === "object";
-	}
-	
-	function isArray(arr){
-		return !isUndefined(arr) && arr !== null && arr.constructor === Array;
-	}
-	
-	try{
-		if(isUndefined(min) || min === null || max === null){
-			return false;
-		}
-		
-		var arr = [];
-		
-		if(!!window.jQuery && min instanceof jQuery && isUndefined(max)){
-			//jQuery object
-			if(min.length > 0){
-				arr = randoSequence(0, min.length - 1);
-				for(var i = 0; i < arr.length; i++){
-					arr[i] = {index:arr[i], value:min.eq(arr[i])};
-				}
-			}
-			return arr;
-		}
-		
-		//load array
-		if(!isUndefined(max)){
-			if(!isNumber(min) || !isNumber(max) || min % 1 > 0 || max % 1 > 0){
-				return false;
-			}
-			//double int
-			if(min > max){
-				var temp = max;
-				max = min;
-				min = temp;
-			}
-			
-			for(var i = min; i <= max; i++){
-				arr[arr.length] = i;
-			}
-		}
-		else if(isArray(min) && isUndefined(max)){
-			//array
-			for(var i = 0; i < min.length; i++){
-				arr[arr.length] = {index:i, value:min[i]};
-			}
-		}
-		else if(isObject(min) && isUndefined(max)){
-			//object
-			for(var prop in min){
-   	 			if(Object.prototype.hasOwnProperty.call(min, prop)){
-   	     			arr[arr.length] = {key:prop, value:min[prop]};
-   	 			}
-			}
-		}
-		else if(isString(min) && isUndefined(max)){
-			//string
-			for(var i = 0; i < min.length; i++){
-				arr[arr.length] = min.charAt(i);
-			}
-		}
-		else if(isNumber(min) && typeof isUndefined(max)){
-			//single int
-			if(min >= 0){
-				return randoSequence(0, min);
-			}
-			return randoSequence(min, 0);
-		}
-		else{
-			return false;
-		}
-		
-		//shuffle array
-		for(i = arr.length - 1; i > 0; i--){
-			var indexToSwapWith = rando(i);
-			var temp = arr[i];
-			arr[i] = arr[indexToSwapWith];
-			arr[indexToSwapWith] = temp;
-		}
-		
-		//return
-		return arr;
-	}
-	catch(err){
-		return false;
-	}
+function randoSequence(arg1, arg2){
+    var isUndefined = (variable) => typeof variable === "undefined",
+    isNumber = (num) => typeof num === "number" && !isNaN(num),
+    isString = (str) => typeof str === "string",
+    isObject = (obj) => typeof obj === "object",
+    isArray = (arr) => !isUndefined(arr) && arr !== null && arr.constructor === Array;
+    
+    try{
+        if(isUndefined(arg1) || arg1 === null || arg2 === null) return false;//invalid arguments
+        
+        var arr = [];
+        
+        if(!!window.jQuery && arg1 instanceof jQuery && isUndefined(arg2)){
+            //jQuery object
+            if(arg1.length > 0){
+                arr = randoSequence(0, arg1.length - 1);
+                for(var i = 0; i < arr.length; i++) arr[i] = {index:arr[i], value:arg1.eq(arr[i])};
+            }
+            return arr;
+        }
+        
+        if(!isUndefined(arg2)){
+            if(!isNumber(arg1) || !isNumber(arg2) || arg1 % 1 > 0 || arg2 % 1 > 0) return false;//invalid arguments
+            
+            //int from min through max (inclusive of both min and max)
+            if(arg1 > arg2) var temp = arg2, arg2 = arg1, arg1 = temp;
+            for(var i = arg1; i <= arg2; i++) arr[arr.length] = i;
+        }
+        else if(isArray(arg1) && isUndefined(arg2)){
+            //array
+            for(var i = 0; i < arg1.length; i++) arr[arr.length] = {index:i, value:arg1[i]};
+        }
+        else if(isObject(arg1) && isUndefined(arg2)){
+            //object
+            for(var prop in arg1) if(Object.prototype.hasOwnProperty.call(arg1, prop)) arr[arr.length] = {key:prop, value:arg1[prop]};
+        }
+        else if(isString(arg1) && isUndefined(arg2)){
+            //string
+            for(var i = 0; i < arg1.length; i++) arr[arr.length] = arg1.charAt(i);
+        }
+        else if(isNumber(arg1) && typeof isUndefined(arg2)){
+            //int from 0 through max OR min through 0 if negative (inclusive of both min and max)
+            return arg1 >= 0 ? randoSequence(0, arg1) : randoSequence(arg1, 0);
+        }
+        else{
+            //invalid arguments
+            return false;
+        }
+        
+        //shuffle values
+        for(i = arr.length - 1; i > 0; i--) var indexToSwapWith = rando(i), temp = arr[i], arr[i] = arr[indexToSwapWith], arr[indexToSwapWith] = temp;
+        
+        return arr;
+    }
+    catch(err) return false;
 }
